3cc61b4cb60e39b27db324df297a7272ada9772e
Rhythm Jain <rhythmjain523@gmail.com>
2026-02-07 21:46:38 +0530

    abu-metha

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,8 @@ import SamagraShikshaPrivacyPolicies from "./components/privacy/samagra_shiksha/
 import SamagraShikshaAccountDeletion from "./components/privacy/samagra_shiksha/accountDeletion";
 import RishavSethiPrivacyPolicies from "./components/privacy/rishav_sethi/privacyPolicy";
 import RishavSethiAccountDeletion from "./components/privacy/rishav_sethi/accountDeletion";
+import AbuMethaPrivacyPolicies from "./components/privacy/abu_metha/privacyPolicy";
+import AbuMethaAccountDeletion from "./components/privacy/abu_metha/accountDeletion";
 
 const App = () => (
   <BrowserRouter>
@@ -31,24 +33,26 @@ const App = () => (
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/blood-connect/privacy-policy" element={<BloodConnectPrivacyPolicies/>} />
-        <Route path="/blood-connect/account-deletion" element={<BloodConnectAccountDeletion/>} />
-        <Route path="/hornbill/privacy-policy" element={<HornbillPrivacyPolicies/>} />
-        <Route path="/hornbill/account-deletion" element={<HornbillAccountDeletion/>} />
-        <Route path="/dyrs/privacy-policy" element={<DYRSPrivacyPolicies/>} />
-        <Route path="/dyrs/account-deletion" element={<DYRSAccountDeletion/>} />
-        <Route path="/kscj/privacy-policy" element={<KSCJPrivacyPolicies/>} />
-        <Route path="/kscj/account-deletion" element={<KSCJAccountDeletion/>} />
-        <Route path="/nbse/privacy-policy" element={<NBSEPrivacyPolicies/>} />
-        <Route path="/nbse/account-deletion" element={<NBSEAccountDeletion/>} />
-        <Route path="/tradikart/privacy-policy" element={<TradikartPrivacyPolicies/>} />
-        <Route path="/tradikart/account-deletion" element={<TradikartAccountDeletion/>} />
-        <Route path="/peace-channel/privacy-policy" element={<PeaceChannelPrivacyPolicies/>} />
-        <Route path="/peace-channel/account-deletion" element={<PeaceChannelAccountDeletion/>} />
-        <Route path="/samagra-shiksha/privacy-policy" element={<SamagraShikshaPrivacyPolicies/>} />
-        <Route path="/samagra-shiksha/account-deletion" element={<SamagraShikshaAccountDeletion/>} />
-        <Route path="/er-rishav-sethi/privacy-policy" element={<RishavSethiPrivacyPolicies/>} />
-        <Route path="/er-rishav-sethi/account-deletion" element={<RishavSethiAccountDeletion/>} />
+        <Route path="/blood-connect/privacy-policy" element={<BloodConnectPrivacyPolicies />} />
+        <Route path="/blood-connect/account-deletion" element={<BloodConnectAccountDeletion />} />
+        <Route path="/hornbill/privacy-policy" element={<HornbillPrivacyPolicies />} />
+        <Route path="/hornbill/account-deletion" element={<HornbillAccountDeletion />} />
+        <Route path="/dyrs/privacy-policy" element={<DYRSPrivacyPolicies />} />
+        <Route path="/dyrs/account-deletion" element={<DYRSAccountDeletion />} />
+        <Route path="/kscj/privacy-policy" element={<KSCJPrivacyPolicies />} />
+        <Route path="/kscj/account-deletion" element={<KSCJAccountDeletion />} />
+        <Route path="/nbse/privacy-policy" element={<NBSEPrivacyPolicies />} />
+        <Route path="/nbse/account-deletion" element={<NBSEAccountDeletion />} />
+        <Route path="/tradikart/privacy-policy" element={<TradikartPrivacyPolicies />} />
+        <Route path="/tradikart/account-deletion" element={<TradikartAccountDeletion />} />
+        <Route path="/peace-channel/privacy-policy" element={<PeaceChannelPrivacyPolicies />} />
+        <Route path="/peace-channel/account-deletion" element={<PeaceChannelAccountDeletion />} />
+        <Route path="/samagra-shiksha/privacy-policy" element={<SamagraShikshaPrivacyPolicies />} />
+        <Route path="/samagra-shiksha/account-deletion" element={<SamagraShikshaAccountDeletion />} />
+        <Route path="/er-rishav-sethi/privacy-policy" element={<RishavSethiPrivacyPolicies />} />
+        <Route path="/er-rishav-sethi/account-deletion" element={<RishavSethiAccountDeletion />} />
+        <Route path="/shri-abu-metha/privacy-policy" element={<AbuMethaPrivacyPolicies />} />
+        <Route path="/shri-abu-metha/account-deletion" element={<AbuMethaAccountDeletion />} />
       </Routes>
       <Footer />
     </div>
